9b724b3b492bf7d7bf1b0e97c17019d465e82d40
DoctorTone <drtonesoftware@gmail.com>
2019-08-23 20:08:50 +0100

    Fix default camera position.

--- src/js/sceneConfig.js
+++ src/js/sceneConfig.js
@@ -5,43 +5,14 @@ const SceneConfig = {
     ambientLightColour: 0x383838,
     pointLightColour: 0xffffff,
     CameraPos: {
-        x: 66,
-        y: 60,
+        x: 0,
+        y: 0,
         z: 75
     },
     LookAtPos: {
-        x: -10,
-        y: -10,
-        z: -15
-    },
-    fireLargePos: {
-        x: 19.5,
-        y: 4,
-        z: 13
-    },
-    fireLargeScale: {
-        x: 3,
-        y: 3,
-        z: 1
-    },
-    fireSmallPos: {
-        x: 3,
-        y: 4,
-        z: 35.5
-    },
-    fireSmallScale: {
-        x: 1.5,
-        y: 1.5,
-        z: 1
-    },
-    WHITEBOARD_WIDTH: 4.5,
-    WHITEBOARD_HEIGHT: 2,
-    WHITEBOARD_DEPTH: 0.25,
-    WHITEBOARD_ROT_Y: Math.PI/2,
-    WhiteBoardPos: {
-        x: 0.15,
-        y: 4.5,
-        z: 30.94
+        x: 0,
+        y: 0,
+        z: 0
     },
     NEAR_PLANE: 0.1,
     FAR_PLANE: 10000,
@@ -49,17 +20,6 @@ const SceneConfig = {
     GroundWidth: 2000,
     GroundHeight: 1000,
     GroundSegments: 16,
-    UserWidth: 5,
-    UserHeight: 20,
-    UserScale: 5,
-    UserRadius: 1,
-    UserSegments: 16,
-    SecondsOffset: 1532900000000,
-    MaxPlaybackSpeed: 16,
-    PosScale: 10,
-    GroundOffset: -4,
-    FloorHeight: 3.25,
-    PointerHeight: 2,
     RIGHT: 0,
     LEFT: 1,
     ZOOM_SPEED: 0.1
